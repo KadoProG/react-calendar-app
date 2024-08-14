45dd76eb8090f2228d6936fe823e033d098f435c
KadoProG <splatrain@gmail.com>
2024-08-14 11:07:35 +0900

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.scss';
 import { MyRouter } from './routes/Router.tsx';
 import { CalendarConfigFormDialogContextProvider } from '@/contexts/CalendarConfigFormDialogContext.tsx';
-import { KeyDownContextProvider } from '@/contexts/KeydownContext.tsx';
+import { KeyDownContextProvider } from '@/contexts/KeyDownContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
