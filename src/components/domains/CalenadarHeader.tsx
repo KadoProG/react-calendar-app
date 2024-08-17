f06d5471514ebe53c7ff6409c81dfd2053613432
KadoProG <splatrain@gmail.com>
2024-08-18 07:14:44 +0900

    #22 カレンダー追加ボタンの追加

--- src/components/domains/CalenadarHeader.tsx
+++ src/components/domains/CalenadarHeader.tsx
@@ -3,6 +3,10 @@ import { Button } from '@/components/common/button/Button';
 import { CalendarConfigContext } from '@/contexts/CalendarConfigContext';
 import styles from '@/components/domains/CalendarHeader.module.scss';
 import { CalendarHeaderDayRows } from '@/components/domains/CalendarHeaderDayRows';
+import { CalendarConfigFormDialogContext } from '@/contexts/CalendarConfigFormDialogContext';
+import { v4 as uuidv4 } from 'uuid';
+import dayjs from 'dayjs';
+import { CalendarEventContext } from '@/contexts/CalendarEventContext';
 
 interface CalenadarHeaderProps {
   setFixedContentHeight: (height: number) => void;
@@ -13,6 +17,8 @@ interface CalenadarHeaderProps {
  */
 export const CalendarHeader: React.FC<CalenadarHeaderProps> = (props) => {
   const fixedContentRef = React.useRef<HTMLDivElement>(null);
+  const { openDialog } = React.useContext(CalendarConfigFormDialogContext);
+  const { addCalendarEvent } = React.useContext(CalendarEventContext);
 
   const { config, baseDate, setBaseDate } = React.useContext(CalendarConfigContext);
 
@@ -30,6 +36,20 @@ export const CalendarHeader: React.FC<CalenadarHeaderProps> = (props) => {
     }
   }, [props]);
 
+  const handleAddCalendar = React.useCallback(async () => {
+    const newCalendarEvent: CalendarEvent = {
+      id: uuidv4(),
+      title: '',
+      start: dayjs().startOf('hour').add(1, 'hour'),
+      end: dayjs().startOf('hour').add(2, 'hour'),
+      isAllDayEvent: false,
+    };
+    const result = await openDialog(newCalendarEvent);
+    if (result.type === 'save') {
+      addCalendarEvent(result.calendarEvent ?? newCalendarEvent);
+    }
+  }, [openDialog, addCalendarEvent]);
+
   React.useEffect(() => {
     updateHeight();
     window.addEventListener('resize', updateHeight);
@@ -44,6 +64,7 @@ export const CalendarHeader: React.FC<CalenadarHeaderProps> = (props) => {
         <p>８月上旬の予定</p>
         <Button onClick={handleBasePrev}>＜</Button>
         <Button onClick={handleBaseNext}>＞</Button>
+        <Button onClick={handleAddCalendar}>＋カレンダーを追加する</Button>
       </div>
       <CalendarHeaderDayRows />
     </div>
