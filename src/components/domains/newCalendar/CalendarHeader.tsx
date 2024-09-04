fe974840cf21773b39ae33415d6f6ee0a73fe6ee
KadoProG <splatrain@gmail.com>
2024-09-04 20:43:17 +0900

    update 関数の分割とスクロールボタンの定義

---
+++ src/components/domains/newCalendar/CalendarHeader.tsx
@@ -0,0 +1,54 @@
+import dayjs from '@/libs/dayjs';
+import { AuthContext } from '@/contexts/AuthContext';
+import { CalendarContext } from '@/contexts/CalendarContext';
+import { formatDateRange } from '@/utils/convertDayjs';
+import React from 'react';
+import { useController, useWatch } from 'react-hook-form';
+import { Button } from '@/components/common/button/Button';
+import { SettingButton } from '@/components/common/button/SettingButton';
+
+export const CalenadarHeader: React.FC = () => {
+  const { user } = React.useContext(AuthContext);
+  const { control } = React.useContext(CalendarContext);
+
+  const start = useWatch({ control, name: 'start' });
+  const end = useWatch({ control, name: 'end' });
+
+  const startController = useController({
+    control,
+    name: 'start',
+  });
+
+  const handleScrollDate = React.useCallback(
+    (type: -1 | 1) => {
+      const prev = dayjs(start).add(7 * type, 'day');
+      startController.field.onChange(prev.toISOString());
+    },
+    [startController, start]
+  );
+
+  const dateText = formatDateRange(dayjs(start), dayjs(end));
+  return (
+    <div style={{ display: 'flex', alignItems: 'center', gap: 4 }}>
+      <div style={{ width: 38, height: 38 }}>
+        <img
+          src="/images/icons/vite.svg"
+          alt="calendar"
+          style={{ width: '100%', height: '100%' }}
+        />
+      </div>
+      <p style={{ fontWeight: 'bold' }}>{dateText}</p>
+      <Button onClick={() => handleScrollDate(-1)}>＜</Button>
+      <Button onClick={() => handleScrollDate(1)}>＞</Button>
+      <SettingButton style={{ marginLeft: 4 }} />
+
+      <div style={{ flex: 1 }} />
+
+      {user && (
+        <Button width={50} style={{ padding: 2 }}>
+          <img src={user?.imageUrl} alt="お前" width={32} height={32} />
+        </Button>
+      )}
+    </div>
+  );
+};
--- src/components/domains/newCalendar/NewCalendar.tsx
+++ src/components/domains/newCalendar/NewCalendar.tsx
@@ -1,29 +1,8 @@
-import dayjs from '@/libs/dayjs';
-import { CalendarContext } from '@/contexts/CalendarContext';
-import { formatDateRange } from '@/utils/convertDayjs';
 import React from 'react';
-import { useWatch } from 'react-hook-form';
-import { Button } from '@/components/common/button/Button';
+import { CalenadarHeader } from '@/components/domains/newCalendar/CalendarHeader';
 
-export const NewCalendar: React.FC = () => {
-  const { control } = React.useContext(CalendarContext);
-
-  const start = useWatch({ control, name: 'start' });
-  const end = useWatch({ control, name: 'end' });
-
-  const dateText = formatDateRange(dayjs(start), dayjs(end));
-  return (
-    <div style={{ display: 'flex', alignItems: 'center', gap: 4 }}>
-      <div style={{ width: 37.5, height: 37.5 }}>
-        <img
-          src="/images/icons/vite.svg"
-          alt="calendar"
-          style={{ width: '100%', height: '100%' }}
-        />
-      </div>
-      <p style={{ fontWeight: 'bold' }}>{dateText}</p>
-      <Button>＜</Button>
-      <Button>＞</Button>
-    </div>
-  );
-};
+export const NewCalendar: React.FC = () => (
+  <div>
+    <CalenadarHeader />
+  </div>
+);
