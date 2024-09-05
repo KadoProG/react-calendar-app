933d8d747bfa5d9801ef3e1435e95a41dc85fd60
KadoProG <splatrain@gmail.com>
2024-09-06 05:50:12 +0900

    update イベントはみ出し分の表示の最適化

--- src/components/domains/newCalendar/CalendarBodyTopRow.tsx
+++ src/components/domains/newCalendar/CalendarBodyTopRow.tsx
@@ -17,13 +17,20 @@ interface CalendarBodyTopRowProps {
  */
 export const CalendarBodyTopRow: React.FC<CalendarBodyTopRowProps> = (props) => {
   // index値から日付を取得
-  const date = React.useMemo(() => dayjs(props.start).add(props.i, 'day'), [props.start, props.i]);
+  const date = React.useMemo(
+    () => dayjs(props.start).startOf('day').add(props.i, 'day'),
+    [props.start, props.i]
+  );
 
   // 終日予定のうち、その日に該当するものを取得
   const calendarEventsInAllDayInDay = React.useMemo(
     () =>
-      props.calendarEventsInAllDay.filter((event) => dayjs(event.start!.date).isSame(date, 'day')),
-    [props.calendarEventsInAllDay, date]
+      props.calendarEventsInAllDay.filter(
+        (event) =>
+          dayjs(event.start!.date).isSame(date, 'day') ||
+          (props.i === 0 && dayjs(event.start!.date).isBefore(date, 'day'))
+      ),
+    [props.calendarEventsInAllDay, date, props.i]
   );
 
   // 選択された日付と同じかどうか
@@ -49,14 +56,16 @@ export const CalendarBodyTopRow: React.FC<CalendarBodyTopRowProps> = (props) => 
       <p style={{ textAlign: 'center' }}>{date.format('ddd')}</p>
       <p style={{ textAlign: 'center' }}>{date.date()}</p>
       {calendarEventsInAllDayInDay.map((event) => {
-        const scheculeDiff = dayjs(event.end!.date).diff(dayjs(event.start!.date), 'day');
+        const startDate = dayjs(event.start!.date);
+        const endDate = dayjs(event.end!.date);
+        const scheculeDiff = endDate.diff(dayjs(date), 'day');
+        const overDiff = !(scheculeDiff + props.i < 7);
+        const resultDiff = overDiff ? 7 - props.i : scheculeDiff + props.i;
         return (
           <div
             key={event.id}
-            className={styles.calendarEvent}
-            style={{
-              width: `${scheculeDiff * 100}%`,
-            }}
+            className={`${styles.calendarEvent} ${startDate.isBefore(date, 'day') ? styles.start : ''} ${overDiff ? styles.end : ''}`}
+            style={{ width: `${resultDiff * 100}%` }}
           >
             {event.summary}
           </div>
