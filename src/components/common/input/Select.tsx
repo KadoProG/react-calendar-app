9c95e779b4b50fccb7c5ba724242ca534913308f
KadoProG <splatrain@gmail.com>
2024-09-15 17:42:33 +0900

    close #50

--- src/components/common/input/Select.tsx
+++ src/components/common/input/Select.tsx
@@ -1,21 +1,28 @@
+import styles from '@/components/common/input/Select.module.scss';
 import { FieldValues, useController, UseControllerProps } from 'react-hook-form';
 
 type SelectProps<T extends FieldValues> = UseControllerProps<T> & {
   label?: string;
   options: { label: string; value: string }[];
   style?: React.CSSProperties;
+  required?: boolean;
 };
 
 export const Select = <T extends FieldValues>(props: SelectProps<T>) => {
-  const { field } = useController<T>({
+  const { field, fieldState } = useController<T>({
     name: props.name,
     control: props.control,
   });
 
   return (
-    <div style={props.style}>
-      {props.label && <label>{props.label}</label>}
-      <select {...field}>
+    <div className={styles.container} style={props.style}>
+      {props.label && (
+        <div>
+          <label htmlFor={props.name}>{props.label}</label>
+          {props.required && <span className={styles.required}>*</span>}
+        </div>
+      )}
+      <select {...field} className={`${styles.Input} ${fieldState.error ? styles.InputError : ''}`}>
         {props.options.map((option) => (
           <option key={option.value} value={option.value}>
             {option.label}
